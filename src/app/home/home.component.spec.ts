e82854cba74e55f117560a0fc044ef7bf92c8eaf
Jefferson-G <70906776+Jefferson-G@users.noreply.github.com>
2023-01-23 10:16:10 -0500

    Update home.component.spec.ts

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
+import { RegistroPagosComponent } from './registro-pagos.component';
 
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+describe('RegistroPagosComponent', () => {
+  let component: RegistroPagosComponent;
+  let fixture: ComponentFixture<RegistroPagosComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ RegistroPagosComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(HomeComponent);
+    fixture = TestBed.createComponent(RegistroPagosComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
