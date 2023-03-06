491a72d35498129a83b8150e63e258b3072bcd1f
Jefferson-G <70906776+Jefferson-G@users.noreply.github.com>
2023-03-06 09:12:48 -0500

    Update form-components.component.spec.ts

--- src/app/form-components/form-components.component.spec.ts
+++ src/app/form-components/form-components.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormComponentsComponent } from './form-components.component';
 
@@ -20,4 +20,46 @@ describe('FormComponentsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`should have as title 'GuamanPruebaU3'`, () => {
+    const fixture = TestBed.createComponent(FormComponentsComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('GuamanPruebaU3');
+  });
+
+  fit('Probar toEqual', () =>{
+    const fixture = TestBed.createComponent(FormComponentsComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.formstyle span')?.textContent).toEqual('Nombres y Apellidos');
+  });
+
+  fit('Probar toBeLessThan', () => {
+    const fixture = TestBed.createComponent(FormComponentsComponent);
+    fixture.detectChanges();
+    const cedulaCtrl = document.getElementById("cedulaCtrl") as HTMLInputElement;
+    expect(cedulaCtrl.value).toBeLessThan(2350574238);
+  })
+
+  fit('Probar toContain', () =>{
+    const fixture = TestBed.createComponent(FormComponentsComponent);
+    fixture.detectChanges();
+    const titulo = fixture.nativeElement as HTMLElement;
+    expect(titulo.textContent).toContain('datos');
+  });
+
+  fit('Probar tomatch', () =>{
+    const fixture = TestBed.createComponent(FormComponentsComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.formstyle span')?.textContent).toMatch("res");
+  });
+
+  fit('Probar toBeNull', () =>{
+    const fixture = TestBed.createComponent(FormComponentsComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as  HTMLElement;
+    expect(compiled.querySelector('.formstyle span')?.contains).toBeNull;
+  });
+
 });
